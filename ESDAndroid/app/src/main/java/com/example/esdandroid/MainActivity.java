9de33718e4168e4513eb670bddb74f9ade4b0368
Magnus Mortensen <Magnus.mortensen@outlook.com>
2019-12-11 10:05:14 +0100

    It fucking works nico! we are gonna be rich!

--- ESDAndroid/app/src/main/java/com/example/esdandroid/MainActivity.java
+++ ESDAndroid/app/src/main/java/com/example/esdandroid/MainActivity.java
@@ -12,6 +12,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,17 +22,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+       /* FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+
+    public void onCreateAlarmButton(View view){
+        EditText editText = findViewById(R.id.editText3);
+        EditText editText2 = findViewById(R.id.editText4);
+        TextView textView = findViewById(R.id.TestField);
+
+        textView.setText("");
+
+        int hours = Integer.parseInt(editText.getText().toString());
+        int minutes = Integer.parseInt(editText2.getText().toString());
+
+        textView.append("Alarm created at " + hours + ":" + minutes );
+
+
+
     }
 
     @Override
